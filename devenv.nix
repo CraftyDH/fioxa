8a1811a5793e60d4cbae87d7b7122d0f46778aed
trickypr <23250792+trickypr@users.noreply.github.com>
2024-07-31 17:19:12 +1000

    Setup a devenv for nix development

---
+++ devenv.nix
@@ -0,0 +1,14 @@
+{ pkgs, lib, config, inputs, ... }:
+
+{
+  # https://devenv.sh/packages/
+  packages = [ pkgs.git ];
+
+  # https://devenv.sh/languages/
+  languages.rust = {
+    enable = true;
+    channel = "nightly";
+    components = [ "rustc" "cargo" "clippy" "rustfmt" "rust-analyzer" "rust-src" ];
+    mold.enable = false;
+  };
+}
