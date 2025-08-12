34a0a77e8237b955adbb2ae88e3280b006073473
sempiternal-aurora <78790545+sempiternal-aurora@users.noreply.github.com>
2025-08-12 15:33:56 +1000

    replace devenv with flake and oxalica rust overlay

---
+++ flake.nix
@@ -0,0 +1,48 @@
+{
+  description = "rust compiler";
+
+  inputs = {
+    rust-overlay.url = "github:oxalica/rust-overlay";
+    rust-overlay.inputs.nixpkgs.follows = "nixpkgs";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs =
+    {
+      nixpkgs,
+      rust-overlay,
+      flake-utils,
+      ...
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        overlays = [ (import rust-overlay) ];
+        pkgs = import nixpkgs {
+          inherit system overlays;
+        };
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          name = "rust";
+          env.OVMF = "${pkgs.OVMF.fd}/FV";
+          packages = [
+            pkgs.git
+            pkgs.qemu_kvm
+          ];
+          buildInputs = [
+            (pkgs.rust-bin.nightly."2025-06-24".default.override {
+              extensions = [
+                "rustc"
+                "cargo"
+                "clippy"
+                "rustfmt"
+                "rust-analyzer"
+                "rust-src"
+              ];
+            })
+          ];
+        };
+      }
+    );
+}
